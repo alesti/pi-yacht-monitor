8b6f39bede4c791d4fa7bd0218cd9e7397388bb3
lnitram <MartinLeuschner@googlemail.com>
2013-10-08 23:33:05 +0200

    handle missing keys

--- server/server.py
+++ server/server.py
@@ -8,10 +8,13 @@ urls = (
 class index:
     def GET(self):
         r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        voltage = "xxx" if r.get("boat.voltage") is None else r.get("boat.voltage") 
+        temperature = "xxx" if r.get("boat.temperature") is None else r.get("boat.temperature")
+        bilge = "xxx" if r.get("boat.bilge") is None else r.get("boat.bilge")
         ret = "<table border='1' cellspacing='0' cellpadding='3'>"
-        ret += "<tr><td>Voltage</td><td>" + r.get("boat.voltage") + "</td></tr>"
-        ret += "<tr><td>Temperature</td><td>" + r.get("boat.temperature") + "</td></tr>"
-        ret += "<tr><td>Bilge</td><td>" + r.get("boat.bilge") + "</td></tr>"
+        ret += "<tr><td>Voltage</td><td>" + voltage + "</td></tr>"
+        ret += "<tr><td>Temperature</td><td>" + temperature + "</td></tr>"
+        ret += "<tr><td>Bilge</td><td>" + bilge + "</td></tr>"
         ret += "</table>"
         return ret
 
