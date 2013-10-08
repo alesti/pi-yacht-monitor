a9f3927e950e37a04490945e06b2de474d769fc0
lnitram <MartinLeuschner@googlemail.com>
2013-10-08 23:27:23 +0200

    server reads values from redis

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 import web
+import redis
 
 urls = (
     '/', 'index'
@@ -6,7 +7,13 @@ urls = (
 
 class index:
     def GET(self):
-        return "Hello, world!"
+        r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        ret = "<table border='1' cellspacing='0' cellpadding='3'>"
+        ret += "<tr><td>Voltage</td><td>" + r.get("boat.voltage") + "</td></tr>"
+        ret += "<tr><td>Temperature</td><td>" + r.get("boat.temperature") + "</td></tr>"
+        ret += "<tr><td>Bilge</td><td>" + r.get("boat.bilge") + "</td></tr>"
+        ret += "</table>"
+        return ret
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
