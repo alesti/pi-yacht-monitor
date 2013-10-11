07ebc919ed46dd5dde9abdd9c465e4f56ed482e4
lnitram <MartinLeuschner@googlemail.com>
2013-10-11 18:01:47 +0200

    ship name can now be configured

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 import web
 import redis
 import os
+from web import form
 
 urls = (
     '/', 'index',
@@ -10,6 +11,10 @@ urls = (
 )
 
 render = web.template.render('templates', base='layout')
+
+
+def get_nameform(boatname):
+    return form.Form(form.Textbox("name",description="Name",value=boatname),form.Button("speichern", type="submit", description="Speichern"))
 
 
 class about:
@@ -31,11 +36,18 @@ class webcam:
         return render.webcam("static/webcam/webcam.jpg")
 
 class config:
+
     def GET(self):
         r = redis.StrictRedis(host='localhost', port=6379, db=0)
-        boatname = "xxx" if r.get("config.boat.name") is None else r.get("config.boat.name") 
-        return render.config(boatname)
-
+        boatname = r.get("config.boat.name")
+        return render.config(get_nameform(boatname))
+    def POST(self):
+        r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        i = web.input()
+        print i
+        boatname=i.name
+        r.set("config.boat.name",boatname) 
+        return render.config(get_nameform(boatname))
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
