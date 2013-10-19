9723fdcb90328c78be7f17f31d9fe7bf63c5c6f2
lnitram <MartinLeuschner@googlemail.com>
2013-10-19 21:50:03 +0200

    changed config datatype to hash

--- pi-yacht-monitor/server/php/httpdocs/config.php
+++ pi-yacht-monitor/server/php/httpdocs/config.php
@@ -3,18 +3,17 @@ require_once("../inc/head.inc.php");
 echo "<h1>Konfiguration</h1>";
 
 if (isset($_POST['smtp_save']) && $_POST['smtp_save'] == 'Speichern') {
-$redis->set("config.email.smtp.server",$_POST['smtp_server']);
-$redis->set("config.email.smtp.username",$_POST['smtp_username']);
-$redis->set("config.email.smtp.password",$_POST['smtp_password']);
-$redis->set("config.email.smtp.sender",$_POST['smtp_sender']);
+$redis->hset("config.email.smtp","server",$_POST['smtp_server']);
+$redis->hset("config.email.smtp","username",$_POST['smtp_username']);
+$redis->hset("config.email.smtp","password",$_POST['smtp_password']);
+$redis->hset("config.email.smtp","sender",$_POST['smtp_sender']);
 echo "<ul><li>Die Einstellungen wurden erfolgreich gespeichert</li></ul>";
 }
 
-
-$smtp_server = $redis->get("config.email.smtp.server");
-$smtp_username = $redis->get("config.email.smtp.username");
-$smtp_password = $redis->get("config.email.smtp.password");
-$smtp_sender = $redis->get("config.email.smtp.sender");
+$smtp_server = $redis->hget("config.email.smtp","server");
+$smtp_username = $redis->hget("config.email.smtp","username");
+$smtp_password = $redis->hget("config.email.smtp","password");
+$smtp_sender = $redis->hget("config.email.smtp","sender");
 
 echo "<h2>E-Mail-Server</h2>";
 echo "Hier werden die Server-Einstellungen für den E-Mail-Versand festgelegt.";
@@ -25,6 +24,17 @@ echo "<tr><td>Username</td><td><input type=\"text\" name=\"smtp_username\" value
 echo "<tr><td>Passwort</td><td><input type=\"password\" name=\"smtp_password\" value =\"$smtp_password\"/></td></tr>";
 echo "<tr><td>E-Mail</td><td><input type=\"text\" name=\"smtp_sender\" value = \"$smtp_sender\"/></td></tr>";
 echo "<tr><td>&nbsp;</td><td><input type=\"submit\" name=\"smtp_save\" value = \"Speichern\"/></td></tr>";
+echo "</table>";
+echo "</form>";
+
+echo "<h2>User-Login</h2>";
+echo "Hier werden Username und Passwort zum Einloggen in diese Weboberfläche festgelegt";
+echo "<form action=\"\" method=\"post\">";
+echo "<table border=\"0\" cellspacing=\"0\" cellpadding=\"5\">";
+echo "<tr><td>Username</td><td><input type=\"text\" name=\"login_user\" value=\"$login_user\"/></td></tr>";
+echo "<tr><td>Passwort</td><td><input type=\"password\" name=\"login_pass_1\"/></td></tr>";
+echo "<tr><td>Passwort</td><td><input type=\"password\" name=\"login_pass_2\"/></td></tr>";
+echo "<tr><td>&nbsp;</td><td><input type=\"submit\" name=\"login_save\" value=\"Speichern\"/></td></tr>";
 echo "</table>";
 echo "</form>";
 
