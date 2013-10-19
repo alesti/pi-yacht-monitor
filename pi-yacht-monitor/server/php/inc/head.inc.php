4f1823610a24191377a9ad38b5c26ce445d4b736
lnitram <MartinLeuschner@googlemail.com>
2013-10-19 19:30:02 +0200

    added charset-info

--- pi-yacht-monitor/server/php/inc/head.inc.php
+++ pi-yacht-monitor/server/php/inc/head.inc.php
@@ -7,6 +7,7 @@ $redis = new Predis\Client();
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
 <html>
 <head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
 <title>Yacht-Monitor</title>
 <link rel="stylesheet" type="text/css" href="css/layout.css">
 </head>
