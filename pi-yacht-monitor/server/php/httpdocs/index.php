e89c60649d78cc093a6c0436208f1f23ded1f647
lnitram <MartinLeuschner@googlemail.com>
2013-10-20 08:41:23 +0200

    show measured data

--- pi-yacht-monitor/server/php/httpdocs/index.php
+++ pi-yacht-monitor/server/php/httpdocs/index.php
@@ -1,6 +1,23 @@
 <?php
 require_once("../inc/head.inc.php");
 echo "<h1>Startseite</h1>";
-echo "Hier kommt mal die &Uuml;bersicht &uuml;ber das System hin";
+
+
+$keys = $redis->keys("boat.*");
+
+echo "<table border=\"1\" cellspacing=\"0\" cellpadding=\"5\">"; 
+
+foreach ($keys as $key) {
+
+  $row = $redis->hgetall($key);
+  echo "<tr>";
+  echo "<td>" . explode(".",$key)[1] . "</td>"; 
+  echo "<td>" . $row["value"] . "</td>";
+  echo "<td>" . date("d.m.Y H:i:s",$row["time"]) . "</td>";
+  echo "</tr>";
+}
+
+echo "</table>";
+
 require_once("../inc/foot.inc.php");
 ?>
