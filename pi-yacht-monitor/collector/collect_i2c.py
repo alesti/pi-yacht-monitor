925b4607ed1e77cfe0828c64d24c195273b18926
RIA-Ingo <ingo.vonitter@freenet.de>
2013-11-03 16:48:22 +0100

    Debugginng in den Daemon eingebaut anständiges Initscript erstellt

--- pi-yacht-monitor/collector/collect_i2c.py
+++ pi-yacht-monitor/collector/collect_i2c.py
@@ -4,19 +4,43 @@ import sys
 import redis
 from smbus import SMBus
 import time
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# create a file handler
+handler = logging.FileHandler('/var/log/i2ccollect.log')
+handler.setLevel(logging.DEBUG)
+
+# create a logging format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(handler)
+
+logger.info('Hello baby')
+
+
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+
 
 
 # reads data from horter-analog-in-card
 # converts it with factor to human-readable voltage
 # save in redis with name from config
 def readPCF8591(config):
-    print "Now reading HorterAnalogIn"
     address = int(config["address"],16)
     busnumber = int(config["bus"])
     bus = SMBus(busnumber)
-    
+    logger.debug('Now reading HorterAnalogIn')
+    logger.debug('Busnumber: ')
+    logger.debug('Address: ')
+
     for i in range(0,4):
         port = "in"+str(i) + "-"
         in_active = config[port + "active"]
@@ -37,13 +61,17 @@ def readPCF8591(config):
 # converts to human-readable temp
 # save in redis with name from config
 def readLM75(config):
-    print "Now reading HorterTemp"
+    logger.debug('--------------------')
+    logger.debug('Now reading LM75')
     address = int(config["address"],16)
     busnumber = int(config["bus"])
     name = config["name"]
     bus = SMBus(busnumber)
+    logger.debug('Busnumber: %s', busnumber)
+    logger.debug('Address: %s', address)
+    logger.debug('Name: %s', name)
     raw_temp = bus.read_word(address)
-    vorkomme = raw_temp & 0xFF
+    vorkomma = raw_temp & 0xFF
     nachkomma = raw_temp >> 15
     temp = 0
     if (vorkomma & 0x80) != 0x80:
@@ -51,10 +79,12 @@ def readLM75(config):
     else:
         vorkomma = -((~vorkomma & 0xFF) + 1)
         temp = vorkomma + nachkomma * 0.5
+    logger.debug('Temperatur: %s', temp)
     storagehandler.save("boat." + name,temp)
 
+
 def readPCF8574_IN(config):
-    print "Now reading DigitalIn"
+    logger('Now reading DigitalIn')
     address = int(config["address"],16)
     busnumber = int(config["bus"])
     bus = SMBus(busnumber)
@@ -64,12 +94,12 @@ def readPCF8574_IN(config):
         in_active = config[port + "active"]
         if in_active == "1":
             in_name = config[port + "name"]
-            value = 1&(state>>i) 
+            value = 1&(state>>i)
             storagehandler.save("boat." + in_name,value)
 
 
 def readPCF8574_OUT(config):
-    print "Now reading DigitalOut"
+    logger.debug('Now reading DigitalOut')
     address = int(config["address"],16)
     busnumber = int(config["bus"])
     bus = SMBus(busnumber)
@@ -83,22 +113,24 @@ def readPCF8574_OUT(config):
             value = 0 if value==1 else 1
             storagehandler.save("boat." + in_name,value)
 
+logger.info('Start reading')
 
 while True:
     # Read all i2c-modules from redis
     i2c_keys = r.keys("config.i2c.*")
-    
+
     for key in i2c_keys:
         m = r.hgetall(key)           # get all modules for i2c
         if (m["active"] == "1"):     # only process when active
             sensor = m["type"]       # read sensor-type
-            
+            logging.debug('sensor ',sensor)
+
             if sensor == "PCF8591":
                 try:
                     readPCF8591(m)
                 except:
                     pass
-     
+
             if sensor == "LM75":
                try:
                    readLM75(m)
@@ -106,16 +138,16 @@ while True:
                    pass
 
             if sensor == "PCF8574_IN":
-               try: 
+               try:
                    readPCF8574_IN(m)
                except:
                    pass
 
             if sensor == "PCF8574_OUT":
-               try:   
+               try:
                   readPCF8574_OUT(m)
                except:
                   pass
-    
             # if sensor unknown => just ignore
+
     time.sleep(10)
