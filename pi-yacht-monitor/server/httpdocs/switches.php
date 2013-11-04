0d9cc649cf5a9d9115a84be85e5e233e50292df1
lnitram <MartinLeuschner@googlemail.com>
2013-11-05 00:53:16 +0100

    show message that switch-task is running

--- pi-yacht-monitor/server/httpdocs/switches.php
+++ pi-yacht-monitor/server/httpdocs/switches.php
@@ -13,7 +13,7 @@ if (isset($_POST["switch"])) {
 }
 
 echo "<table border='1' cellspacing='0' cellpadding='5'>";
-echo "<tr><th>Name</th><th>Output Typ</th><th>Port</th><th>Name</th><th>Istwert</th><th>Aktion</th></tr>";
+echo "<tr><th>Name</th><th>Output Typ</th><th>Port</th><th>Name</th><th>Istwert</th><th>Sollwert</th><th>Aktion</th></tr>";
 foreach ($keys as $key) {
   $v = $redis->hgetall($key);
   $type = $v["type"];
@@ -21,9 +21,11 @@ foreach ($keys as $key) {
   if ($type == "PCF8574_OUT_INV") {
     for ($i = 0; $i < 8; $i++) {
        $value = $redis->hget("boat." .$v["in" . $i . "-name"] , "value");
+       $givenvalue = $v["in" . $i . "-givenvalue"];
        $text = $value == 0? "an":"aus";
-       $givenvalue = $value == 0?1:0;
+       $newvalue = $value == 0?1:0;
        $subkey = "in" . $i . "-givenvalue";
+      
        echo "<form action='' method='post'/>";
        echo "<tr>";
        echo "<td>$name</td>";
@@ -31,10 +33,16 @@ foreach ($keys as $key) {
        echo "<td>$i</td>";
        echo "<td>" . $v["in" . $i . "-name"] . "</td>";
        echo "<td>$value</td>";
-       echo "<td><input type='submit' name='switch' value='$text'/>";
+       echo "<td>" . $givenvalue . "</td>";
+       echo "<td>";
+       if ($value != $givenvalue) {
+         echo "wird geschaltet";
+       } else {
+         echo "<input type='submit' name='switch' value='$text'/>";
+       }
        echo "<input type='hidden' name='key' value='$key'/>";
        echo "<input type='hidden' name='subkey' value='$subkey'/>";
-       echo "<input type='hidden' name='givenvalue' value='$givenvalue'/>";
+       echo "<input type='hidden' name='givenvalue' value='$newvalue'/>";
        echo "</td>";
        echo "</tr>";
        echo "</form>";
